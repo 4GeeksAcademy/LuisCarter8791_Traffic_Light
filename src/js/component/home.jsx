c58753d30321c92a40a0a89d6db629aa6fc984cc
Luis Carter <46801420+LuisCarter8791@users.noreply.github.com>
2024-08-14 10:14:51 +0000

    Project complete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,35 +1,28 @@
 import React, { useState } from "react";
-import "..styles/home.css";
-
-
 //create your first component
 const Home = () => {
-	const [activeLight, setActiveLight] = useState ("Red");
-	function handleClick(e) {
-		e.preventDefault()
-		if (activeLight == "Red"){
-			setActiveLight("Yellow");
-		}
-		else if (activeLight == "Yellow"){
-			setActiveLight("Green");
-		}
-		else {
-			setActiveLight("Red")
-		}
-	}
-
-	return (
-		<div className="main">
-			
-			<div>
-				<div className={activeLight === "Red" ? "Red Light Glow red-light" : "Red Light"} onClick={() => setActiveLight ("Red")}></div>
-				<div className={activeLight === "Yellow" ? "Yellow Light Glow yellow-light" : "Yellow Light"} onClick={() => setActiveLight ("Yellow")}></div>
-				<div className={activeLight === "Green" ? "Green Light Glow green-light" : "Green Light"} onClick={() => setActiveLight ("Green")}></div>
-			</div>
-			<button onClick={(e) => handleClick (e)} className="btn">Button </button>
-
-		</div>
-	);
+    const [activeLight, setActiveLight] = useState ("Red");
+    function handleClick(e) {
+        e.preventDefault()
+        if (activeLight == "Red"){
+            setActiveLight("Yellow");
+        }
+        else if (activeLight == "Yellow"){
+            setActiveLight("Green");
+        }
+        else {
+            setActiveLight("Red")
+        }
+    }
+    return (
+        <div className="main">
+            <div>
+                <div className={activeLight === "Red" ? "Red Light Glow" : "Red Light"} onClick={() => setActiveLight ("Red")}></div>
+                <div className={activeLight === "Yellow" ? " Yellow Light Glow " : "Yellow Light"} onClick={() => setActiveLight ("Yellow")}></div>
+                <div className={activeLight === "Green" ? " Green Light Glow " : "Green Light"} onClick={() => setActiveLight ("Green")}></div>
+            </div>
+            <button onClick={(e) => handleClick (e)} className="btn">Button </button>
+        </div>
+    );
 };
-
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,10 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 // include your styles into the webpack bundle
 // import "../styles/index.css";
-import "src/styles/home.css";
+import "../styles/home.css";
 //import your own components
 import Home from "./component/home.jsx";
-
 //render your react application
 ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
-
